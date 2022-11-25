0370db7731f3f203c9cdd2638848e86dd80f1543
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2022-11-25 08:30:08 +0100

    feat: fullscreening app

--- shortcuts.go
+++ shortcuts.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 )
 
 func (a *App) loadShortCuts() {
 	a.mainWin.Canvas().SetOnTypedKey(func(k *fyne.KeyEvent) {
-		fmt.Println(k)
+		// fmt.Println(k)
 
-		if k.Name == "Escape" {
+		// TODO: convert to switch?
+		if k.Name == fyne.KeyEscape {
+			// close app
 			a.mainWin.Close()
+		} else if k.Name == fyne.KeyF11 {
+			// full screen app
+			if a.mainWin.FullScreen() {
+				a.mainWin.SetFullScreen(false)
+			} else {
+				a.mainWin.SetFullScreen(true)
+			}
 		}
 	})
-
 }
